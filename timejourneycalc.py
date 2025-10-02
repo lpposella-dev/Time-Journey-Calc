d861f4dfa7dd3ecae52e504a0b294d07298f12e2
lpposella-dev <lpposella2@gmail.com>
2025-10-02 15:28:07 -0300

    timejourneycalc.py

--- timejourneycalc.py
+++ timejourneycalc.py
@@ -1,3 +1,5 @@
+import time
+
 class Dayhour:
     
     def __init__(self, entr, salm, valm, said):
@@ -8,9 +10,26 @@ class Dayhour:
         self.said_hora = (said)
         
         self.hora_feita_dia = Timecalc.sum_hour(Timecalc.subtract_hour(self.salm_hora, self.entr_hora), Timecalc.subtract_hour(self.said_hora, self.valm_hora))
-    
+        
     def __str__(self):
-        return f"Horas trabalhadas no dia: \"{self.hora_feita_dia}\""
+        return self.hora_feita_dia
+        
+    def __repr__(self):
+        return self.hora_feita_dia
+    
+class Monthcalc:
+    
+    def __init__(self, month_logs: list):
+        init_time = "00:00"
+        for month_day_time in month_logs:
+            init_time = Timecalc.sum_hour(init_time, month_day_time)
+        self.final_time = init_time
+            
+    def __str__(self):
+        return str(self.final_time)
+        
+    def __repr__(self):
+        return str(self.final_time)
     
 class Timecalc:
     
@@ -55,21 +74,49 @@ class Timecalc:
             
         return f"{general_hour:02}:{general_min:02}"
         
-menuopt = 0
-
-while menuopt != 3:
-    menuopt = int(input("1 - Calcular Dia\n2 - Calcular Mes\n3 - Sair\n"))
+class Menu:
     
-    if menuopt > 3 or menuopt <= 0:  #error handler
-        print("Escolha uma opção do menu!")
+    def __init__(self):
         menuopt = 0
-    else:
-        if menuopt == 1:
-            entr = input("\nQue horas voce chegou no trabalho? ")
-            salm = input("\nQue horas voce saiu para almoço? ")
-            valm = input("\nQue horas voce voltou do almoço? ")
-            said = input("\nQue horas voce foi embora do trabalho? ")
-        
-            print(f"\nNeste dia voce trabalhou: {Dayhour(entr, salm, valm, said)} horas\n")
-            time.sleep(2)
-            menuopt = 0
+
+        while menuopt != 3:
+            menuopt = int(input("1 - Calcular Dia\n2 - Calcular Mes\n3 - Sair\n"))
+    
+            if menuopt > 3 or menuopt <= 0:  #error handler
+                print("Escolha uma opção do menu!")
+                menuopt = 0
+            else:
+                if menuopt == 1:
+                    print(f"\nNeste dia voce trabalhou: {self.calc_day()} horas\n")
+                    time.sleep(2)
+                    menuopt = 0
+            
+                if menuopt == 2:
+                    month_hours = []
+                    menuopt_2 = 0
+                    while menuopt_2 != 3:
+                        menuopt_2 = int(input("1 - Incluir dia\n2 - Pular dia\n3 - Fechar mes\n"))
+                
+                        if menuopt_2 == 1:
+                            
+                            current_day = self.calc_day()
+                            month_hours.append(str(current_day))
+                            #print(month_hours) just for verify
+                            
+                        elif menuopt_2 == 2:
+                            
+                            month_hours.append("00:00")
+                            #print(month_hours) just for verify
+                            
+                        elif menuopt_2 == 3:
+                            print(f"O total de horas trabalhadas neste mes foi de: {Monthcalc(month_hours)}\n")
+                            
+    def calc_day(self):
+                        entr = input("\nQue horas voce chegou no trabalho? ")
+                        salm = input("\nQue horas voce saiu para almoço? ")
+                        valm = input("\nQue horas voce voltou do almoço? ")
+                        said = input("\nQue horas voce foi embora do trabalho? ")
+                
+                        return Dayhour(entr, salm, valm, said)
+                        
+Menu()
